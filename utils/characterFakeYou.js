78e0a9b4e45abe7dccde54d11e18d469601ffed4
Alberto <info@peralstudio.com>
2023-01-21 17:10:07 +0100

    Added array of character fakeyou api voices

---
+++ utils/characterFakeYou.js
@@ -0,0 +1,70 @@
+module.exports = [
+    {
+        name: "illojuan",
+        value: "TM:q8vnck5cpb6t",
+    },
+    {
+        name: "elxokas",
+        value: "TM:8tk8hcjx6m48",
+    },
+    {
+        name: "chiquito",
+        value: "TM:9yzxw5pvh45e",
+    },
+    {
+        name: "skippy",
+        value: "TM:zce2jy1wj3kw",
+    },
+    {
+        name: "bugsbunny",
+        value: "TM:nehyxnzeyjjd",
+    },
+    {
+        name: "yoda",
+        value: "TM:4qawq2jn91e7",
+    },
+    {
+        name: "sonic",
+        value: "TM:vacctt4krw1k",
+    },
+    {
+        name: "auronyoutube",
+        value: "TM:jgv6d8br5jdr",
+    },
+    {
+        name: "aurontwitch",
+        value: "TM:d7mz6jzmb6jj",
+    },
+    {
+        name: "vegeta777",
+        value: "TM:20frsmyt6vbw",
+    },
+    {
+        name: "2pac",
+        value: "TM:jv2j06zg7vh0",
+    },
+    {
+        name: "bobesponja",
+        value: "TM:2djcdhdmag9t",
+    },
+    {
+        name: "bartsimpson",
+        value: "TM:86jdn5w5xbyk",
+    },
+    {
+        name: "homersimpson",
+        value: "TM:q1sxefjbgw7y",
+    },
+    {
+        name: "apu",
+        value: "TM:b0m61vw1rw1m",
+    },
+    {
+        name: "matiasprats",
+        value: "TM:xmgs6d585xk9",
+    },
+    {
+        name: "doraemon",
+        value: "TM:9b7sezbhr15d",
+    },
+];
