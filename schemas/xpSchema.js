65c7a6cb879df1b511c7d5dd756294f6b2ea6360
Alberto <info@peralstudio.com>
2023-01-24 01:49:20 +0100

    Added canvacord

---
+++ schemas/xpSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const xpSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        require: true,
+    },
+    guildID: {
+        type: String,
+        require: true,
+    },
+    userID: {
+        type: String,
+        require: true,
+    },
+    xp: {
+        type: Number,
+        default: 0,
+    },
+    level: {
+        type: Number,
+        default: 1,
+    },
+});
+
+module.exports = mongoose.model("userxp", xpSchema);
