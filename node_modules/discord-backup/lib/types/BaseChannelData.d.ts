e2d18b566e6fc0ccf69658c7b75f3659b5622f74
Alberto <info@peralstudio.com>
2023-02-02 20:44:54 +0100

    Added discord-backup

---
+++ node_modules/discord-backup/lib/types/BaseChannelData.d.ts
@@ -0,0 +1,8 @@
+import { TextBasedChannelTypes, VoiceBasedChannelTypes, ThreadChannelType } from 'discord.js';
+import { ChannelPermissionsData } from './';
+export interface BaseChannelData {
+    type: TextBasedChannelTypes | VoiceBasedChannelTypes | ThreadChannelType;
+    name: string;
+    parent?: string;
+    permissions: ChannelPermissionsData[];
+}
