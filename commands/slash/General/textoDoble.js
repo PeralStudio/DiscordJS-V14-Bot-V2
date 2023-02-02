ad79e11622d8b2083c59798b8910473585793121
Alberto <info@peralstudio.com>
2023-02-02 21:07:41 +0100

    Added textoDoble command

---
+++ commands/slash/General/textoDoble.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "texto-doble",
+    description: "Crear Texto Doble.",
+    type: 1,
+    options: [
+        {
+            type: 3,
+            name: "texto",
+            description: "Texto a crear.",
+            required: true,
+        },
+    ],
+    permissions: {
+        DEFAULT_MEMBER_PERMISSIONS: "SendMessages",
+    },
+    run: async (client, interaction, config) => {
+        const texto = interaction.options.get("texto").value;
+
+        const res = await fetch(`https://api.popcatdev.repl.co/doublestruck?text=${texto}`);
+        const response = await res.json();
+
+        await interaction.reply({ content: response.text });
+    },
+};
