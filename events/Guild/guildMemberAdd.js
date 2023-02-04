8dc3c4d28e05cebdb79003206cd55d2f378b1bb3
Alberto <info@peralstudio.com>
2023-02-04 22:28:49 +0100

    Added logs

--- events/Guild/guildMemberAdd.js
+++ events/Guild/guildMemberAdd.js
@@ -7,6 +7,7 @@ module.exports = {
 };
 
 client.on("guildMemberAdd", async (member) => {
+    console.log("member: ", member);
     if (member.guild.id === process.env.GUILD_ID) {
         const imagesArray = [
             "https://i.postimg.cc/C5kLJdsD/istockphoto-1164222265-612x612.png",
