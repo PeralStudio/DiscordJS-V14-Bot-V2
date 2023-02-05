8354d8462e83efa0f97a59e46f78de233ada0372
Alberto <info@peralstudio.com>
2023-02-05 18:11:49 +0100

    Added roleCreate Event

---
+++ events/Guild/roleCreate.js
@@ -0,0 +1,37 @@
+const { AuditLogEvent, EmbedBuilder } = require("discord.js");
+const client = require("../../index");
+require("dotenv").config();
+
+module.exports = {
+    name: "roleCreate.js",
+};
+
+client.on("roleCreate", async (message) => {
+    await message.guild
+        .fetchAuditLogs({
+            type: AuditLogEvent.RoleCreate,
+        })
+        .then(async (audit) => {
+            const authorRole = audit.entries.first().executor;
+
+            const channel = await message.guild.channels.cache.get(process.env.LOGS_CHANNEL_ID);
+
+            const embed = new EmbedBuilder()
+                .setAuthor({
+                    name: `Rol Creado por ${authorRole.tag}`,
+                    iconURL: authorRole.displayAvatarURL({ dynamic: true }),
+                })
+                .addFields({
+                    name: ` `,
+                    value: "El rol `" + message.name + "` ha sido creado en " + message.guild.name,
+                })
+                .setFooter({
+                    text: `Autor: ${authorRole.id} | Canal ID: ${message.id}`,
+                    iconURL: client.user.displayAvatarURL(),
+                })
+                .setTimestamp()
+                .setColor("#04A350");
+
+            channel.send({ embeds: [embed] });
+        });
+});
