d9d734cb6b66a789ca308a4942eea4a93d115812
Alberto <info@peralstudio.com>
2023-02-05 18:11:17 +0100

    Added channelDelete Event

---
+++ events/Guild/channelDelete.js
@@ -0,0 +1,55 @@
+const { AuditLogEvent, EmbedBuilder } = require("discord.js");
+const client = require("../../index");
+require("dotenv").config();
+
+module.exports = {
+    name: "channelDelete.js",
+};
+
+client.on("channelDelete", async (channel) => {
+    channel.guild
+        .fetchAuditLogs({
+            type: AuditLogEvent.ChannelDelete,
+        })
+        .then(async (audit) => {
+            const channelLogs = await channel.guild.channels.cache.get(process.env.LOGS_CHANNEL_ID);
+            const { executor } = audit.entries.first();
+
+            let type = channel.type;
+
+            if (type == 0) type = `Texto`;
+            if (type == 2) type = `Voz`;
+            if (type == 13) type = `Stage`;
+            if (type == 15) type = `Foro`;
+            if (type == 5) type = `Announcememnt`;
+            if (type == 4) type = `Categoria`;
+
+            const embed = new EmbedBuilder()
+                .setAuthor({
+                    name: `❌ Canal borrado por ${executor.tag}`,
+                    iconURL: executor.displayAvatarURL({ dynamic: true }),
+                })
+                .addFields(
+                    {
+                        name: ` `,
+                        value: `Nombre del canal: **${channel.name}**`,
+                    },
+                    {
+                        name: ` `,
+                        value: `Tipo de canal: **${type}**`,
+                    },
+                    {
+                        name: ` `,
+                        value: `ID del canal: **${channel.id}**`,
+                    }
+                )
+                .setFooter({
+                    text: `Autor: ${executor.id}`,
+                    iconURL: client.user.displayAvatarURL(),
+                })
+                .setTimestamp()
+                .setColor("#FF0000");
+
+            channelLogs.send({ embeds: [embed] });
+        });
+});
