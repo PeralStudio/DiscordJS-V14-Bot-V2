d70aa418080593195fae612419aa76a3868cbceb
Alberto <info@peralstudio.com>
2023-02-05 18:11:31 +0100

    Added messageDelete Event

---
+++ events/Guild/messageDelete.js
@@ -0,0 +1,47 @@
+const { AuditLogEvent, EmbedBuilder } = require("discord.js");
+const client = require("../../index");
+require("dotenv").config();
+
+module.exports = {
+    name: "messageDelete.js",
+};
+
+client.on("messageDelete", async (message) => {
+    await message.guild
+        .fetchAuditLogs({
+            type: AuditLogEvent.MessageDelete,
+        })
+        .then(async (audit) => {
+            const authorDelete = audit.entries.first().executor;
+
+            if (!message.content) return;
+            if (message.author.bot) return;
+            if (authorDelete.bot) return;
+
+            const channel = await message.guild.channels.cache.get(process.env.LOGS_CHANNEL_ID);
+
+            const embed = new EmbedBuilder()
+                .setAuthor({
+                    name: `🗑️ Borrado por por ${authorDelete.tag}`,
+                    iconURL: message.author.displayAvatarURL({ dynamic: true }),
+                })
+                .addFields(
+                    {
+                        name: ` `,
+                        value: `Mensaje de ${message.author} ha sido borrado en ${message.channel}`,
+                    },
+                    {
+                        name: `${message.content}`,
+                        value: ` `,
+                    }
+                )
+                .setFooter({
+                    text: `Autor: ${message.author.id} | Mensaje ID: ${message.id}`,
+                    iconURL: client.user.displayAvatarURL(),
+                })
+                .setTimestamp()
+                .setColor("#FF0000");
+
+            channel.send({ embeds: [embed] });
+        });
+});
