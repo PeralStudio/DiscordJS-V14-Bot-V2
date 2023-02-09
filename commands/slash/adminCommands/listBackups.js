417c7840b4c663acc58fb3bf3275660e2d10858a
Alberto <66646508+PeralStudio@users.noreply.github.com>
2023-02-09 03:26:05 +0100

    Create listBackups command

---
+++ commands/slash/adminCommands/listBackups.js
@@ -0,0 +1,62 @@
+const { EmbedBuilder } = require("discord.js");
+const backup = require("discord-backup");
+const path = require("path");
+require("dotenv").config();
+
+module.exports = {
+    name: "list-backup",
+    description: "Lista de backups",
+    type: 1,
+    permissions: {
+        DEFAULT_MEMBER_PERMISSIONS: "SendMessages",
+    },
+    run: async (client, interaction, config) => {
+        if (interaction.user.id !== process.env.ID_OWNER) {
+            interaction.reply({
+                ephemeral: true,
+                embeds: [
+                    new EmbedBuilder().setDescription("⛔ No tienes permisos.").setColor("#EA3939"),
+                ],
+            });
+            return;
+        }
+
+        let backupsArray;
+
+        backup.setStorageFolder(path.join(__dirname, "../../../backups"));
+        await backup.list().then((backups) => {
+            backupsArray = backups;
+        });
+
+        const bakcupsList = backupsArray
+            .map((backup) => ":open_file_folder: `" + backup + "`")
+            .join("\n\n");
+
+        console.log(backupsArray.length);
+
+        if (backupsArray.length > 0) {
+            const embed = new EmbedBuilder()
+                .setTitle(`Backups disponibles`)
+                .setDescription(`${bakcupsList}`)
+                .setColor("#D4B053")
+                .setTimestamp()
+                .setFooter({
+                    text: process.env.NAME_BOT,
+                    iconURL: client.user.displayAvatarURL(),
+                });
+
+            interaction.reply({ embeds: [embed] });
+        } else {
+            const embed = new EmbedBuilder()
+                .setTitle(`⛔ No hay backups disponibles`)
+                .setColor("#D4B053")
+                .setTimestamp()
+                .setFooter({
+                    text: process.env.NAME_BOT,
+                    iconURL: client.user.displayAvatarURL(),
+                });
+
+            interaction.reply({ embeds: [embed] });
+        }
+    },
+};
