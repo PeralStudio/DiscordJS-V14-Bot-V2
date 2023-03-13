582651843ae495c4af5d2240fc76bb9d56326d8f
Alberto <66646508+PeralStudio@users.noreply.github.com>
2023-03-13 20:40:17 +0100

    ✨ feat : Add gpt-3.5-turbo

--- commands/slash/General/chatgpt.js
+++ commands/slash/General/chatgpt.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder } = require("discord.js");
 require("dotenv").config();
-const axios = require("axios");
+// const axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
 
 module.exports = {
     name: "chatgpt",
@@ -18,22 +19,34 @@ module.exports = {
         DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
     },
     run: async (client, interaction, config) => {
-        const text = interaction.options.get("texto").value;
+        const prompt = interaction.options.get("texto").value;
 
-        const options = {
-            method: "POST",
-            url: "https://you-chat-gpt.p.rapidapi.com/",
-            headers: {
-                "content-type": "application/json",
-                "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-                "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com"
-            },
-            data: { question: text, max_response_time: 15 }
-        };
+        // const options = {
+        //     method: "POST",
+        //     url: "https://you-chat-gpt.p.rapidapi.com/",
+        //     headers: {
+        //         "content-type": "application/json",
+        //         "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+        //         "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com"
+        //     },
+        //     data: { question: text, max_response_time: 15 }
+        // };
 
         const getData = async () => {
-            let response = await axios.request(options);
-            return response.data.answer;
+            const configuration = new Configuration({
+                apiKey: process.env.OPENAI_API_KEY
+            });
+            const openai = new OpenAIApi(configuration);
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: prompt }]
+            });
+
+            const text = completion.data.choices[0].message.content;
+            return text;
+
+            // let response = await axios.request(options);
+            // return response.data.answer;
         };
 
         await interaction.deferReply({ content: "Cargando...", ephemeral: true });
@@ -42,7 +55,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             // .setThumbnail("https://cdn-icons-png.flaticon.com/512/281/281776.png")
             .setDescription(
-                `**Entrada:**\n\`\`\`\n${text}\`\`\`\n**Respuesta:**\n\`\`\`\n${result}\`\`\``
+                `**Entrada:**\n\`\`\`\n${prompt}\`\`\`\n**Respuesta:**\n\`\`\`\n${result}\`\`\``
             )
             .setFooter({
                 text: process.env.NAME_BOT,
