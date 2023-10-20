4587e7df470ba2e0fa00d990e6bb206dde55b246
Alberto <66646508+PeralStudio@users.noreply.github.com>
2023-10-20 22:29:48 +0200

    ✨ feat : Add Bard-Goggle to CHAT_GPT_CHANNEL

--- events/Guild/messageCreate.js
+++ events/Guild/messageCreate.js
@@ -1,7 +1,6 @@
-const { EmbedBuilder, PermissionsBitField, WebhookClient } = require("discord.js");
-const { Configuration, OpenAIApi } = require("openai");
+const { EmbedBuilder, WebhookClient } = require("discord.js");
 const client = require("../../index");
-const config = require("../../config/config.js");
+const axios = require("axios");
 require("dotenv").config();
 
 const webhook = new WebhookClient({
@@ -13,6 +12,7 @@ module.exports = {
 };
 
 client.on("messageCreate", async (message) => {
+    // Send md's bot msg receive to the channel BOT_DMS_CHANNEL
     if (!message.guild) {
         const embed = new EmbedBuilder()
             .setAuthor({
@@ -34,110 +34,26 @@ client.on("messageCreate", async (message) => {
         });
     }
 
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY
-    });
-    const openai = new OpenAIApi(configuration);
-
+    // Interaction chatGPT in CHAT_GPT_CHANNEL channel
     if (message.channel.id === process.env.CHAT_GPT_CHANNEL) {
         if (message.author.bot) return;
-        let conversationLog = [{ role: "system", content: "You are a friendly chatbot." }];
+
+        const options = {
+            method: "GET",
+            url: "https://google-bard1.p.rapidapi.com/",
+            headers: {
+                psid: process.env.BARD_PSID,
+                text: message.content,
+                "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+                "X-RapidAPI-Host": "google-bard1.p.rapidapi.com"
+            }
+        };
 
         try {
             await message.channel.sendTyping();
+            const response = await axios.request(options);
 
-            let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-            prevMessages.reverse();
-
-            prevMessages.forEach((msg) => {
-                if (message.content.startsWith("!")) return;
-                if (msg.author.id !== client.user.id && message.author.bot) return;
-                if (msg.author.id !== message.author.id) return;
-
-                conversationLog.push({
-                    role: "user",
-                    content: msg.content
-                });
-            });
-
-            const result = await openai
-                .createChatCompletion({
-                    model: "gpt-3.5-turbo",
-                    messages: conversationLog
-                    // max_tokens: 256, // limit token usage
-                })
-                .catch((error) => {
-                    console.log(`OPENAI ERR: ${error}`);
-                });
-
-            message.reply(result.data.choices[0].message);
-        } catch (error) {
-            console.log(`ERR: ${error}`);
-        }
-    }
-
-    if (message.channel.type !== 0) return;
-    if (message.author.bot) return;
-
-    const prefix = config.Prefix || "/";
-
-    if (!message.content.startsWith(config.Prefix)) return;
-    if (!message.guild) return;
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (cmd.length == 0) return;
-
-    let command = client.prefix_commands.get(cmd);
-
-    if (!command) return;
-
-    if (command) {
-        if (command.permissions) {
-            if (
-                !message.member.permissions.has(
-                    PermissionsBitField.resolve(command.permissions || [])
-                )
-            )
-                return message.reply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setDescription(
-                                `🚫 Unfortunately, you are not authorized to use this command.`
-                            )
-                            .setColor("Red")
-                    ]
-                });
-        }
-
-        if ((command.owner, command.owner == true)) {
-            if (config.Users?.OWNERS) {
-                const allowedUsers = []; // New Array.
-
-                config.Users.OWNERS.forEach((user) => {
-                    const fetchedUser = message.guild.members.cache.get(user);
-                    if (!fetchedUser) return allowedUsers.push("*Unknown User#0000*");
-                    allowedUsers.push(`${fetchedUser.user.tag}`);
-                });
-
-                if (!config.Users.OWNERS.some((ID) => message.member.id.includes(ID)))
-                    return message.reply({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setDescription(
-                                    `🚫 Sorry but only owners can use this command! Allowed users:\n**${allowedUsers.join(
-                                        ", "
-                                    )}**`
-                                )
-                                .setColor("Red")
-                        ]
-                    });
-            }
-        }
-
-        try {
-            command.run(client, message, args, prefix, config);
+            message.reply(response.data.response);
         } catch (error) {
             console.error(error);
         }
