27800b056d5614ceed9dcef37cecc775b331efc8
Alberto <66646508+PeralStudio@users.noreply.github.com>
2023-10-30 21:05:34 +0100

    ✨ feat : Add BBDD command Admin

--- commands/slash/adminCommands/bbdd.js
+++ commands/slash/adminCommands/bbdd.js
@@ -6,19 +6,60 @@ module.exports = {
     name: "bbdd",
     description: `Ver todas las colecciones de la BBDD`,
     type: 1,
+    options: [
+        {
+            type: 3,
+            name: "collenctionname",
+            description: "Nombre de la colección."
+        }
+    ],
     permissions: {
         DEFAULT_MEMBER_PERMISSIONS: "SendMessages"
     },
     run: async (client, interaction, config) => {
         if (interaction.user.id !== process.env.ID_OWNER) {
             interaction.reply({
-                ephemeral: true,
                 embeds: [
                     new EmbedBuilder()
                         .setDescription("⛔ No tienes permisos para usar este comando.")
                         .setColor("#EA3939")
-                ]
+                ],
+                ephemeral: true
             });
+            return;
+        }
+
+        const collectionName = interaction.options.getString("collenctionname");
+
+        if (collectionName) {
+            const collectionsLength = await connection.collections[collectionName].countDocuments();
+            const collectionItems = await connection.collections[collectionName].find({});
+            const collectionItemsData = await collectionItems.toArray();
+
+            const embedCollections = new EmbedBuilder()
+                .setTitle(`Colección **${collectionName}**`)
+                .setDescription(`\`${collectionsLength} Documentos\``)
+                .setColor("#0099ff")
+                .setThumbnail(
+                    "https://i.blogs.es/577c8b/650_1000_mongo_bumper.sh-600x600/1366_2000.png"
+                )
+                .setTimestamp()
+                .setFooter({
+                    text: process.env.NAME_BOT,
+                    iconURL: client.user.displayAvatarURL()
+                });
+
+            for (const item of collectionItemsData) {
+                embedCollections.addFields({
+                    name: "Documento",
+                    value: `\`\`\`${JSON.stringify(item, null, 2)}\`\`\``
+                });
+            }
+            interaction.reply({
+                embeds: [embedCollections],
+                ephemeral: true
+            });
+
             return;
         }
 
