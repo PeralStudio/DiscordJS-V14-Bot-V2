ef677c3a16afacf11c3cc4df65db5b5c7200f0a0
Alberto <66646508+PeralStudio@users.noreply.github.com>
2024-09-18 21:33:24 +0200

    ✨ feat : Add custom levels & colors

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,39 @@
 const { createLogger, format, transports } = require("winston");
 require("winston-daily-rotate-file");
 
+// Define niveles de log personalizados
+const customLevels = {
+    levels: {
+        title: 0,
+        error: 1,
+        warn: 2,
+        success: 3,
+        info: 4,
+        verbose: 5,
+        gold: 6,
+        red: 6,
+        debug: 7
+    },
+    colors: {
+        title: "bold magenta",
+        error: "bold red",
+        warn: "bold yellow",
+        success: "bold green",
+        info: "bold cyan",
+        verbose: "bold blue",
+        gold: "bold yellow",
+        red: "red yellow",
+        debug: "bold gray"
+    }
+};
+
+require("winston").addColors(customLevels.colors);
+
 // Función para obtener la fecha y hora local en el formato DD-MM-YYYY HH:mm:ss
 const localTimestamp = format.timestamp({
     format: () => {
         const now = new Date();
 
-        // Opciones para formatear la fecha y hora
         const options = {
             day: "2-digit",
             month: "2-digit",
@@ -25,8 +52,10 @@ const localTimestamp = format.timestamp({
     }
 });
 
+// Crea el logger con niveles personalizados y colores
 const logger = createLogger({
-    level: "info",
+    levels: customLevels.levels,
+    level: "debug",
     format: format.combine(
         localTimestamp,
         format.printf((info) => `${info.timestamp} [${info.level.toUpperCase()}] - ${info.message}`)
@@ -34,13 +63,13 @@ const logger = createLogger({
     transports: [
         new transports.DailyRotateFile({
             filename: "logs/bot-%DATE%.log",
-            datePattern: "YYYY-MM-DD",
+            datePattern: "DD-MM-YYYY",
             maxFiles: "7d",
             zippedArchive: true
         }),
         new transports.Console({
             format: format.combine(
-                format.colorize(),
+                format.colorize({ all: true }),
                 format.printf(
                     (info) => `${info.timestamp} [${info.level.toUpperCase()}] - ${info.message}`
                 )
